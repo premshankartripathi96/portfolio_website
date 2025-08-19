d31c7fed55e8b3ffa95280f3f9aead92276246ff
prem shnkar tripathi <pstripathi2020@gmail.com>
2025-08-19 06:03:24 +0530

    styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+    <div className="w-11/12 max-w-3xl text-center mx-auto min-h-screen pt-24 flex flex-col items-center justify-center gap-4">
       <div>
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
       </div>
